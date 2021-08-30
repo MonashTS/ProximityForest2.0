b9a3fc1be47761d8fcf41e4f9b140d24a4303e5c
HerrmannM <HerrmannM@users.noreply.github.com>
2021-08-30 14:33:43 +1000

    Simplify WDTW univariate testing

--- libtempo/test/distance/univariate/wdtw.cpp
+++ libtempo/test/distance/univariate/wdtw.cpp
@@ -3,13 +3,71 @@
 #include <catch.hpp>
 #include <libtempo/distance/wdtw.hpp>
 
-#include "references/dtw/wdtw.hpp"
 #include "../mock/mockseries.hpp"
 
 using namespace mock;
 using namespace libtempo::distance;
 constexpr size_t nbitems = 500;
 constexpr size_t nbweights = 5;
+
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+// Reference
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+namespace reference {
+
+  using namespace libtempo::utils;
+
+  /** Reimplementation of the "modified logistic weight function (MLWF)" from the original paper
+   * "Weighted dynamic time warping for time series classification"
+   * @param i Index of the point in [1..m] (m=length of the sequence)
+   * @param mc Mid point of the sequence (m/2)
+   * @param g "Controls the level of penalization for the points with larger phase difference".
+   *        range [0, +inf), usually in [0.01, 0.6].
+   *        Some examples:
+   *        * 0: constant weight
+   *        * 0.05: nearly linear weights
+   *        * 0.25: sigmoid weights
+   *        * 3: two distinct weights between half sequences
+   * @param wmax Upper bound for the weight parameter. Keep it to 1
+   * @return
+   */
+  inline double mlwf(double i, double mc, double g, double wmax=1){
+    return wmax/(1+std::exp(-g*(i - mc)));
+  }
+
+  /// Reference implementation on a matrix
+  double wdtw_matrix(const vector<double>& series1, const vector<double>& series2, const vector<double>& weights) {
+    const long length1 = to_signed(series1.size());
+    const long length2 = to_signed(series2.size());
+
+    // Check lengths. Be explicit in the conditions.
+    if (length1==0 && length2==0) { return 0; }
+    if (length1==0 && length2!=0) { return PINF<double>; }
+    if (length1!=0 && length2==0) { return PINF<double>; }
+    // Matrix
+    vector<std::vector<double>> matrix(length1, std::vector<double>(length2, 0));
+    // First value
+    matrix[0][0] = weights[0]*square_dist(series1[0], series2[0]);
+    // First line
+    for (long i = 1; i<length2; i++) {
+      matrix[0][i] = matrix[0][i-1]+weights[i]*square_dist(series1[0], series2[i]);
+    }
+    // First column
+    for (long i = 1; i<length1; i++) {
+      matrix[i][0] = matrix[i-1][0]+weights[i]*square_dist(series1[i], series2[0]);
+    }
+    // Matrix computation
+    for (long i = 1; i<length1; i++) {
+      for (long j = 1; j<length2; j++) {
+        const auto d = weights[abs(i-j)]*square_dist(series1[i], series2[j]);
+        const auto v = min(matrix[i][j-1], std::min(matrix[i-1][j], matrix[i-1][j-1]))+d;
+        matrix[i][j] = v;
+      }
+    }
+    return matrix[length1-1][length2-1];
+  }
+
+}
 
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // Testing

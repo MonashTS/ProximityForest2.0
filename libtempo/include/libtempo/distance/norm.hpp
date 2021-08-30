f9cc46757e2342109138da2d5b3c41197cc3a2e9
HerrmannM <HerrmannM@users.noreply.github.com>
2021-08-30 14:01:59 +1000

    Adding norm - univariate tests

---
+++ libtempo/include/libtempo/distance/norm.hpp
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <libtempo/utils/utils.hpp>
+#include <libtempo/distance/distance.hpp>
+
+#include <cmath>
+
+namespace libtempo::distance {
+
+  namespace internal {
+
+    /** Usual norm, Euclidean distance like distance with early abandoning. No pruning.
+     * Only defined for same length series (return +INF if different length).
+     * @tparam FloatType  The floating number type used to represent the series.
+     * @tparam FDist      Distance computation function, must be a (size_t, size_t)->FloatType
+     * @param length1     Length of the first series.
+     * @param length2     Length of the second series.
+     * @param dist        Distance function, has to capture the series as it only gets the (li,co) coordinate
+     * @param cutoff.     Attempt to prune computation of alignments with cost > cutoff.
+     *                    May lead to early abandoning.
+     * @return Norm according to 'dist' between the two series, or +INF if early abandoned.
+     */
+    template<typename FloatType, typename FDist>
+    [[nodiscard]] inline FloatType norm(size_t length1, size_t length2, FDist dist, const FloatType cutoff) {
+      // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+      constexpr auto PINF = utils::PINF<FloatType>;
+      // Check sizes. If both series are empty, return 0, else if one is empty and not the other, maximal error.
+      if(length1 != length2) { return PINF;}
+      if(length1 == 0){return 0;}
+      // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+      // Create a new tighter upper bounds (most commonly used in the code).
+      // First, take the "next float" after "cutoff" to deal with numerical instability.
+      // Then, subtract the cost of the last alignment.
+      // Adjust the lower bound, taking the last alignment into account
+      const FloatType lastA = dist(length1-1, length1-1);
+      const FloatType ub = std::nextafter(cutoff, PINF) - lastA;
+      // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+      // Compute the Euclidean-like distance up to, excluding, the last alignment
+      double cost = 0;
+      for (size_t i{0}; i < length1-1; ++i) { // Stop before the last: counted in the bound!
+        cost += dist(i, i);
+        if(cost>ub){return PINF;}
+      }
+      // Add the last alignment and check the result
+      cost+=lastA;
+      if(cost>cutoff){return PINF;} else { return cost; }
+    }
+
+
+    /** Usual norm, Euclidean distance like distance. No early abandoning. No pruning.
+     * Only defined for same length series (return +INF if different length).
+     * @tparam FloatType  The floating number type used to represent the series.
+     * @tparam FDist      Distance computation function, must be a (size_t, size_t)->FloatType
+     * @param length1     Length of the first series.
+     * @param length2     Length of the second series.
+     * @param dist        Distance function, has to capture the series as it only gets the (li,co) coordinate
+     * @return Norm according to 'dist' between the two series
+     */
+    template<typename FloatType, typename FDist>
+    [[nodiscard]] inline FloatType norm(size_t length1, size_t length2, FDist dist) {
+      // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+      // Max error on different length.
+      if (length1 != length2) { return utils::PINF<FloatType>; }
+      // Compute the Euclidean-like distance
+      FloatType cost = 0.0;
+      for (size_t i{0}; i < length1; ++i) { cost += dist(i, i); }
+      return cost;
+    }
+  } // End of namespace internal
+
+
+
+
+  /** Norm according to a point-to-point distance. Can early abandon but not prune.
+   * Return +INF if the series have different length.
+   * @tparam FloatType  The floating number type used to represent the series.
+   * @tparam FDist      Distance computation function, must be a (size_t, size_t)->FloatType
+   * @param length1     Length of the first series.
+   * @param length2     Length of the second series.
+   * @param dist        Distance function, has to capture the series as it only gets the (li,co) coordinates
+   * @param ub          Upper bound. Attempt to prune computation of alignments with cost > cutoff.
+   *                    May lead to early abandoning.
+   *                    ub = PINF: no lower bounding
+   *                    ub = QNAN: no lower bounding
+   *                    ub = other value: use for pruning and early abandoning
+   * @return Norm according to 'dist' between the two series
+  */
+  template<typename FloatType, typename FDist>
+  [[nodiscard]] FloatType norm(size_t length1, size_t length2, FDist dist, FloatType ub) {
+    if (std::isinf(ub) || std::isnan(ub)) { return internal::norm<FloatType, FDist>(length1, length2, dist); }
+    else { return internal::norm<FloatType, FDist>(length1, length2, dist, ub); }
+  }
+
+  /// Helper with a distance builder 'mkdist'
+  template<typename FloatType>
+  [[nodiscard]] inline FloatType norm(const auto& s1, const auto& s2, auto mkdist, FloatType ub) {
+    return norm(s1.size(), s2.size(), mkdist(s1, s2), ub);
+  }
+
+  /// Helper with the sqed as the default distance builder, and a default ub at +INF
+  template<typename FloatType>
+  [[nodiscard]] inline FloatType norm(const auto& s1, const auto& s2, FloatType ub = utils::PINF<FloatType>) {
+    return norm(s1.size(), s2.size(), distance::sqed<FloatType>(s1, s2), ub);
+  }
+
+  /// Multidimensional helper, with a distance builder 'mkdist'
+  template<typename FloatType>
+  [[nodiscard]] inline FloatType norm(const auto& s1, const auto& s2, size_t ndim, auto mkdist, FloatType ub) {
+    return norm(s1.size()/ndim, s2.size()/ndim, mkdist(s1, s2, ndim), ub);
+  }
+
+  /// Multidimensional helper, with a distance builder 'mkdist'
+  template<typename FloatType>
+  [[nodiscard]] inline FloatType norm(const auto& s1, const auto& s2, size_t ndim, FloatType ub = utils::PINF<FloatType>) {
+    return norm(s1.size()/ndim, s2.size()/ndim, distance::sqed<FloatType>(s1, s2, ndim), ub);
+  }
+
+
+} // End of namespace libtempo::distance
---
+++ libtempo/test/distance/univariate/norm.cpp
@@ -0,0 +1,105 @@
+#define CATCH_CONFIG_FAST_COMPILE
+#include <catch.hpp>
+#include <libtempo/distance/norm.hpp>
+
+#include "../mock/mockseries.hpp"
+
+using namespace mock;
+using namespace libtempo::distance;
+constexpr size_t nbitems = 500;
+
+namespace reference {
+
+  template <typename V>
+  double norm(const V& s1, const V& s2){
+    if(s1.size() != s2.size()){return libtempo::utils::PINF<double>;}
+    double cost = 0;
+    for(size_t i=0; i<s1.size(); ++i){
+      const auto d = s1[i] - s2[i];
+      cost += d*d;
+    }
+    return cost;
+  }
+
+}
+
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+// Testing
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+TEST_CASE("Univariate NORM Fixed length", "[norm][univariate]") {
+  // Setup univariate with fixed length
+  Mocker mocker;
+
+  const auto fset = mocker.vec_randvec(nbitems);
+
+  SECTION("NORM(s,s) == 0") {
+    for (const auto &s: fset) {
+      const double norm_ref_v = reference::norm(s, s);
+      REQUIRE(norm_ref_v == 0);
+
+      const auto norm_v = norm<double>(s, s);
+      REQUIRE(norm_v == 0);
+    }
+  }
+
+  SECTION("NORM(s1, s2)") {
+    for (size_t i = 0; i < nbitems-1; ++i) {
+      const auto &s1 = fset[i];
+      const auto &s2 = fset[i + 1];
+
+      const double norm_ref_v = reference::norm(s1, s2);
+      INFO("Exact same operation order. Expect exact floating point equality.")
+
+      const auto norm_eap_v = norm<double>(s1, s2);
+      REQUIRE(norm_ref_v == norm_eap_v);
+    }
+  }
+
+  SECTION("NN1 NORM"){
+    // Query loop
+    for(size_t i=0; i<nbitems; i+=3) {
+      const auto& s1 = fset[i];
+      // Ref Variables
+      size_t idx_ref=0;
+      double bsf_ref = lu::PINF<double>;
+      // Base Variables
+      size_t idx=0;
+      double bsf = lu::PINF<double>;
+      // EAP Variables
+      size_t idx_eap = 0;
+      double bsf_eap = lu::PINF<double>;
+
+      // NN1 loop
+      for (size_t j = 0; j < nbitems; j+=5) {
+        // Skip self.
+        if(i==j){continue;}
+        const auto& s2 = fset[j];
+
+        // --- --- --- --- --- --- --- --- --- --- --- ---
+        const double v_ref = reference::norm(s1, s2);
+        if (v_ref < bsf_ref) {
+          idx_ref = j;
+          bsf_ref = v_ref;
+        }
+
+        // --- --- --- --- --- --- --- --- --- --- --- ---
+        const auto v = norm<double>(s1, s2);
+        if (v < bsf) {
+          idx = j;
+          bsf = v;
+        }
+
+        REQUIRE(idx_ref == idx);
+
+        // --- --- --- --- --- --- --- --- --- --- --- ---
+        const auto v_eap = norm<double>(s1, s2, bsf_eap);
+        if (v_eap < bsf_eap) {
+          idx_eap = j;
+          bsf_eap = v_eap;
+        }
+
+        REQUIRE(idx_ref == idx_eap);
+      }
+    }// End query loop
+  }// End section
+}

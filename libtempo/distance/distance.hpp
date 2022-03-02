d27474a96a735ac46419ddaaa7cbb48703507d92
HerrmannM <HerrmannM@users.noreply.github.com>
2022-03-02 15:12:10 +1100

    adding different namespace for cost function distance (todo: rename that file!)

--- libtempo/distance/distance.hpp
+++ libtempo/distance/distance.hpp
@@ -9,37 +9,54 @@
 
 namespace libtempo::distance {
 
-  /// Univariate Absolute difference exponent 1
-  template<Float F, Subscriptable D>
-  auto ad1(const D& lines, const D& cols) {
-    return [lines, cols](size_t i, size_t j) {
-      const F d = lines[i]-cols[j];
-      return std::abs(d);
-    };
+  namespace univariate {
+
+    /// CFunBuilder Univariate Absolute difference exponent 1
+    template<Float F, Subscriptable D>
+    auto ad1(const D& lines, const D& cols) {
+      return [lines, cols](size_t i, size_t j) {
+        const F d = lines[i]-cols[j];
+        return std::abs(d);
+      };
+    }
+
+    /// CFunBuilder Univariate Absolute difference exponent 2
+    template<Float F, Subscriptable D>
+    auto ad2(const D& lines, const D& cols) {
+      return [lines, cols](size_t i, size_t j) {
+        const F d = lines[i]-cols[j];
+        return d*d;
+      };
+    }
+
+    /// CFunBuilder Univariate Absolute difference exponent e
+    template<Float F, Subscriptable D>
+    auto ade(const D& lines, const D& cols, const F e) {
+      return [lines, cols, e](size_t i, size_t j) {
+        const F d = lines[i]-cols[j];
+        return std::pow(d, e);
+      };
+    }
+
   }
 
-  /// Univariate Absolute difference exponent 2
-  template<Float F, Subscriptable D>
-  auto ad2(const D& lines, const D& cols) {
-    return [lines, cols](size_t i, size_t j) {
-      const F d = lines[i]-cols[j];
-      return d*d;
-    };
-  }
+  namespace multivariate {
 
-  /// Multivariate Absolute difference exponent 2
-  template<Float F, Subscriptable D>
-  auto ad2N(const D& lines, const D& cols, size_t ndim) {
-    return [lines, cols, ndim](size_t i, size_t j) {
-      const size_t li_offset = i*ndim;
-      const size_t co_offset = j*ndim;
-      F acc{0};
-      for (size_t k{0}; k<ndim; ++k) {
-        const F d = lines[li_offset+k]-cols[co_offset+k];
-        acc += d*d;
-      }
-      return acc;
-    };
+    /// CFunBuilder Multivariate Absolute difference exponent 2
+    template<Float F, Subscriptable D>
+    auto ad2N(const D& lines, const D& cols, size_t ndim) {
+      return [lines, cols, ndim](size_t i, size_t j) {
+        const size_t li_offset = i*ndim;
+        const size_t co_offset = j*ndim;
+        F acc{0};
+        for (size_t k{0}; k<ndim; ++k) {
+          const F d = lines[li_offset+k]-cols[co_offset+k];
+          acc += d*d;
+        }
+        return acc;
+      };
+    }
+
   }
 
   namespace internal {

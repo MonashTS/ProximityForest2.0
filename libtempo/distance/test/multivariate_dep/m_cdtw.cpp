6b812dd8c11ce5f94d5aaffdb193dfa5092add03
HerrmannM <HerrmannM@users.noreply.github.com>
2022-03-02 16:46:19 +1100

    Up multivariate test for CDTW

--- libtempo/distance/test/multivariate_dep/m_cdtw.cpp
+++ libtempo/distance/test/multivariate_dep/m_cdtw.cpp
@@ -4,11 +4,12 @@
 #include <mock/mockseries.hpp>
 #include <libtempo/distance/cdtw.hpp>
 
-
 using namespace mock;
 using namespace libtempo::distance;
-constexpr size_t nbitems = 500;
+using namespace libtempo::distance::multivariate;
+constexpr size_t nbitems = 1000;
 constexpr size_t ndim = 3;
+constexpr double INF = libtempo::utils::PINF<double>;
 
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // Reference
@@ -98,6 +99,8 @@ TEST_CASE("Multivariate Dependent CDTW Fixed length", "[cdtw][multivariate]") {
   // Setup univariate with fixed length
   Mocker mocker;
   mocker._dim = ndim;
+  const auto l = mocker._fixl;
+  const auto l1 = mocker._fixl*ndim;
   const auto& wratios = mocker.wratios;
 
   const auto fset = mocker.vec_randvec(nbitems);
@@ -110,7 +113,7 @@ TEST_CASE("Multivariate Dependent CDTW Fixed length", "[cdtw][multivariate]") {
         const double cdtw_ref_v = cdtw_matrix(s, s, ndim, w);
         REQUIRE(cdtw_ref_v==0);
 
-        const auto cdtw_v = cdtw<double>(s, s, ndim, w);
+        const double cdtw_v = cdtw<double>(l, l, w, ad2N<double>(s, s, ndim), INF);
         REQUIRE(cdtw_v==0);
       }
     }
@@ -128,7 +131,7 @@ TEST_CASE("Multivariate Dependent CDTW Fixed length", "[cdtw][multivariate]") {
         {
           const double cdtw_ref_v = cdtw_matrix(s1, s2, 1, w);
           const double cdtw_ref_uni_v = cdtw_matrix_uni(s1, s2, w);
-          const auto cdtw_tempo_v = cdtw<double>(s1, s2, 1, w);
+          const auto cdtw_tempo_v = cdtw(l1, l1, w, ad2N<double>(s1, s2, 1), INF);
           REQUIRE(cdtw_ref_v==cdtw_ref_uni_v);
           REQUIRE(cdtw_ref_v==cdtw_tempo_v);
         }
@@ -138,7 +141,7 @@ TEST_CASE("Multivariate Dependent CDTW Fixed length", "[cdtw][multivariate]") {
           const double cdtw_ref_v = cdtw_matrix(s1, s2, ndim, w);
           INFO("Exact same operation order. Expect exact floating point equality.")
 
-          const auto cdtw_tempo_v = cdtw<double>(s1, s2, ndim, w);
+          const auto cdtw_tempo_v = cdtw(l, l, w, ad2N<double>(s1, s2, ndim), INF);
           REQUIRE(cdtw_ref_v==cdtw_tempo_v);
         }
       }
@@ -177,7 +180,7 @@ TEST_CASE("Multivariate Dependent CDTW Fixed length", "[cdtw][multivariate]") {
           }
 
           // --- --- --- --- --- --- --- --- --- --- --- ---
-          const auto v = cdtw<double>(s1, s2, ndim, w);
+          const auto v = cdtw(l, l, w, ad2N<double>(s1, s2, ndim), INF);
           if (v<bsf) {
             idx = j;
             bsf = v;
@@ -186,7 +189,7 @@ TEST_CASE("Multivariate Dependent CDTW Fixed length", "[cdtw][multivariate]") {
           REQUIRE(idx_ref==idx);
 
           // --- --- --- --- --- --- --- --- --- --- --- ---
-          const auto v_tempo = cdtw<double>(s1, s2, ndim, w, bsf_tempo);
+          const auto v_tempo = cdtw(l, l, w, ad2N<double>(s1, s2, ndim), bsf_tempo);
           if (v_tempo<bsf_tempo) {
             idx_tempo = j;
             bsf_tempo = v_tempo;
@@ -212,7 +215,7 @@ TEST_CASE("Multivariate Dependent CDTW Variable length", "[cdtw][multivariate]")
         const double cdtw_ref_v = cdtw_matrix(s, s, ndim, w);
         REQUIRE(cdtw_ref_v==0);
 
-        const auto cdtw_v = cdtw<double>(s, s, ndim, w);
+        const auto cdtw_v = cdtw<double>(s.size()/ndim, s.size()/ndim, w, ad2N<double>(s, s, ndim), INF);
         REQUIRE(cdtw_v==0);
       }
     }
@@ -229,7 +232,7 @@ TEST_CASE("Multivariate Dependent CDTW Variable length", "[cdtw][multivariate]")
         {
           const double cdtw_ref_v = cdtw_matrix(s1, s2, 1, w);
           const double cdtw_ref_uni_v = cdtw_matrix_uni(s1, s2, w);
-          const auto cdtw_tempo_v = cdtw<double>(s1, s2, 1, w);
+          const auto cdtw_tempo_v = cdtw<double>(s1.size(), s2.size(), w, ad2N<double>(s1, s2, 1), INF);
           REQUIRE(cdtw_ref_v==cdtw_ref_uni_v);
           REQUIRE(cdtw_ref_v==cdtw_tempo_v);
         }
@@ -239,7 +242,7 @@ TEST_CASE("Multivariate Dependent CDTW Variable length", "[cdtw][multivariate]")
           const double cdtw_ref_v = cdtw_matrix(s1, s2, ndim, w);
           INFO("Exact same operation order. Expect exact floating point equality.")
 
-          const auto cdtw_tempo_v = cdtw<double>(s1, s2, ndim, w);
+          const auto cdtw_tempo_v = cdtw<double>(s1.size()/ndim, s2.size()/ndim, w, ad2N<double>(s1, s2, ndim), INF);
           REQUIRE(cdtw_ref_v==cdtw_tempo_v);
         }
       }
@@ -277,7 +280,7 @@ TEST_CASE("Multivariate Dependent CDTW Variable length", "[cdtw][multivariate]")
           }
 
           // --- --- --- --- --- --- --- --- --- --- --- ---
-          const auto v = cdtw<double>(s1, s2, ndim, w);
+          const auto v = cdtw<double>(s1.size()/ndim, s2.size()/ndim, w, ad2N<double>(s1, s2, ndim), INF);
           if (v<bsf) {
             idx = j;
             bsf = v;
@@ -286,7 +289,7 @@ TEST_CASE("Multivariate Dependent CDTW Variable length", "[cdtw][multivariate]")
           REQUIRE(idx_ref==idx);
 
           // --- --- --- --- --- --- --- --- --- --- --- ---
-          const auto v_tempo = cdtw<double>(s1, s2, ndim, w, bsf_tempo);
+          const auto v_tempo = cdtw<double>(s1.size()/ndim, s2.size()/ndim, w, ad2N<double>(s1, s2, ndim), bsf_tempo);
           if (v_tempo<bsf_tempo) {
             idx_tempo = j;
             bsf_tempo = v_tempo;

7792b3b73200c05f72b12ce1b8d42b345be15fa1
HerrmannM <HerrmannM@users.noreply.github.com>
2022-04-07 15:07:25 +1000

    Update splitters

--- libtempo/_cmake/dummy.cpp
+++ libtempo/_cmake/dummy.cpp
@@ -20,7 +20,6 @@ using namespace std;
 using namespace libtempo;
 using PRNG = std::mt19937_64;
 
-
 void derivative(const double *series, size_t length, double *out) {
   if (length>2) {
     for (size_t i{1}; i<length - 1; ++i) {
@@ -98,16 +97,19 @@ int main(int argc, char **argv) {
     fs::path adiac_test = dirpath/(name + "_TEST.ts");
     auto test_dataset = load_dataset(adiac_test);
 
-
-    /*
-    auto derives = transform::derive(train_dataset, 3);
+    auto derives = transform::derive(train_dataset, 2);
     auto d1 = std::move(derives[0]);
     auto d2 = std::move(derives[1]);
-    auto d3 = std::move(derives[2]);
     cout << d1.name() << endl;
     cout << d2.name() << endl;
-    cout << d3.name() << endl;
 
+    auto test_derives = transform::derive(test_dataset, 2);
+    auto test_d1 = std::move(test_derives[0]);
+    auto test_d2 = std::move(test_derives[1]);
+    cout << test_d1.name() << endl;
+    cout << test_d2.name() << endl;
+
+    /*
     for (size_t i = 0; i<train_dataset.size(); ++i) {
 
       const auto& ts = train_dataset.data()[i];
@@ -154,17 +156,18 @@ int main(int argc, char **argv) {
       public pf::IStrain<L, PFState, PFState>,
       public pf::TimeSeriesDatasetHeader<PFState, F, L> {
 
+      std::map<std::string, size_t> selected_distances;
+
       size_t depth{0};
 
       /// Pseudo random number generator: use a unique pointer (stateful)
       std::unique_ptr<PRNG> prng;
 
       /// Dictionary of name->dataset of time series
-      std::shared_ptr<pf::DatasetMap_t<F,L>> dataset_shared_map;
+      std::shared_ptr<pf::DatasetMap_t<F, L>> dataset_shared_map;
 
-      PFState(size_t seed, std::shared_ptr<pf::DatasetMap_t<F,L>> dataset_shared_map)
-      :prng(std::make_unique<PRNG>(seed)), dataset_shared_map(std::move(dataset_shared_map))
-      { }
+      PFState(size_t seed, std::shared_ptr<pf::DatasetMap_t<F, L>> dataset_shared_map)
+        : prng(std::make_unique<PRNG>(seed)), dataset_shared_map(std::move(dataset_shared_map)) {}
 
       /// Ensure we do not copy the state by error: we have to properly deal with the random number generator
       PFState(const PFState&) = delete;
@@ -172,38 +175,41 @@ int main(int argc, char **argv) {
     private:
 
       /// Cloning Constructor: create a new PRNG
-      PFState(size_t depth, size_t new_seed, std::shared_ptr<pf::DatasetMap_t<F,L>> map):
-      depth(depth), prng(std::make_unique<PRNG>(new_seed)), dataset_shared_map(std::move(map))
-      { }
+      PFState(size_t depth, size_t new_seed, std::shared_ptr<pf::DatasetMap_t<F, L>> map) :
+        depth(depth), prng(std::make_unique<PRNG>(new_seed)), dataset_shared_map(std::move(map)) {}
 
       /// Forking Constructor: transmit PRNG into the new state
-      PFState(size_t depth, std::unique_ptr<PRNG>&& m_prng, std::shared_ptr<pf::DatasetMap_t<F,L>> map):
-      depth(depth), prng(std::move(m_prng)), dataset_shared_map(std::move(map))
-      { }
+      PFState(size_t depth, std::unique_ptr<PRNG>&& m_prng, std::shared_ptr<pf::DatasetMap_t<F, L>> map) :
+        depth(depth), prng(std::move(m_prng)), dataset_shared_map(std::move(map)) {}
 
     public:
 
       /// Transmit the prng down the branch
       PFState branch_fork(size_t /* bidx */) override {
-        return PFState(depth+1, std::move(prng), dataset_shared_map);
+        return PFState(depth + 1, std::move(prng), dataset_shared_map);
       }
 
-      /// Merge "other" into "this". Move the prng into this.
+      /// Merge "other" into "this". Move the prng into this. Merge statistics into this
       void branch_merge(PFState&& other) override {
         prng = std::move(other.prng);
-      }
+        for (const auto&[n, c] : other.selected_distances) {
+          selected_distances[n] += c;
+        }
 
+      }
 
       /// Clone at the forest level - clones must be fully independent as they can be used in parallel
       /// Create a new prng
       std::unique_ptr<PFState> forest_clone() override {
         size_t new_seed = (*prng)();
-        return std::unique_ptr<PFState>( new PFState(depth, new_seed, dataset_shared_map) );
+        return std::unique_ptr<PFState>(new PFState(depth, new_seed, dataset_shared_map));
       }
 
       /// Merge in this a state that has been produced by forest_clone
-      void forest_merge(std::unique_ptr<PFState> /* other */ ) override {
-
+      void forest_merge(std::unique_ptr<PFState> other) override {
+        for (const auto&[n, c] : other->selected_distances) {
+          selected_distances[n] += c;
+        }
       }
 
     };
@@ -222,29 +228,88 @@ int main(int argc, char **argv) {
 
     auto transformations = std::make_shared<classifier::pf::DatasetMap_t<F, L>>();
     transformations->insert({"default", train_dataset});
+    transformations->insert({"d1", d1});
+    transformations->insert({"d2", d2});
     seed = 15;
     PFState train_state = PFState(seed, transformations);
 
     auto test_transformations = std::make_shared<classifier::pf::DatasetMap_t<F, L>>();
     test_transformations->insert({"default", test_dataset});
-    size_t test_seed = seed+5;
+    test_transformations->insert({"d1", test_d1});
+    test_transformations->insert({"d2", test_d2});
+    size_t test_seed = seed + 5;
     PFState test_state = PFState(test_seed, test_transformations);
 
 
     // --- --- --- Parameters range
     auto exponents = std::make_shared<std::vector<double>>(std::vector<double>{0.25, 0.33, 0.5, 1, 2, 3, 4});
-    auto tnames = std::make_shared<std::vector<std::string>>(std::vector<std::string>{"default"});
+    auto tnames = std::make_shared<std::vector<std::string>>(std::vector<std::string>{"default", "d1"}); // , "d2"});
+    auto msm_costs = std::make_shared<std::vector<double>>(
+      std::vector<double>{
+        0.01, 0.01375, 0.0175, 0.02125, 0.025, 0.02875, 0.0325, 0.03625, 0.04, 0.04375,
+        0.0475, 0.05125, 0.055, 0.05875, 0.0625, 0.06625, 0.07, 0.07375, 0.0775, 0.08125,
+        0.085, 0.08875, 0.0925, 0.09625, 0.1, 0.136, 0.172, 0.208, 0.244, 0.28, 0.316, 0.352,
+        0.388, 0.424, 0.46, 0.496, 0.532, 0.568, 0.604, 0.64, 0.676, 0.712, 0.748, 0.784,
+        0.82, 0.856, 0.892, 0.928, 0.964, 1, 1.36, 1.72, 2.08, 2.44, 2.8, 3.16, 3.52, 3.88,
+        4.24, 4.6, 4.96, 5.32, 5.68, 6.04, 6.4, 6.76, 7.12, 7.48, 7.84, 8.2, 8.56, 8.92, 9.28,
+        9.64, 10, 13.6, 17.2, 20.8, 24.4, 28, 31.6, 35.2, 38.8, 42.4, 46, 49.6, 53.2, 56.8,
+        60.4, 64, 67.6, 71.2, 74.8, 78.4, 82, 85.6, 89.2, 92.8, 96.4, 100
+      }
+    );
 
-    // --- --- --- Node Generator
-    auto sg_1nn_da = std::make_shared<pf::SG_1NN_DA<F, L, Strain, Stest>>(tnames, exponents);
-    auto sg_1nn_dtwf = std::make_shared<pf::SG_1NN_DTWFull<F, L, Strain, Stest>>(tnames, exponents);
-    auto sg_1nn_dtw = std::make_shared<pf::SG_1NN_DTW<F, L, Strain, Stest>>(tnames, exponents);
-    auto sg_1nn_wdtw = std::make_shared<pf::SG_1NN_WDTW<F, L, Strain, Stest>>(tnames, exponents);
-    auto sg_1nn_erp = std::make_shared<pf::SG_1NN_ERP<F, L, Strain, Stest>>(tnames, exponents);
+    /** TWE nu parameters */
+    auto twe_nus = std::make_shared<std::vector<double>>(
+      std::vector<double>{0.00001, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1}
+    );
+
+    /** TWE lambda parameters */
+    auto twe_lambdas = std::make_shared<std::vector<double>>(
+      std::vector<double>{0, 0.011111111, 0.022222222, 0.033333333, 0.044444444, 0.055555556, 0.066666667, 0.077777778,
+                          0.088888889, 0.1}
+    );
+
+    // --- --- --- 1NN Elastic Distance Node Generator
+    auto sg_1nn_da =
+      std::make_shared<pf::SG_1NN_DA<F, L, Strain, Stest>>(tnames, exponents);
+
+    auto sg_1nn_adtw =
+      std::make_shared<pf::SG_1NN_ADTW<F, L, Strain, Stest>>(tnames, exponents, 2000, 20, 4);
+
+    auto sg_1nn_dtwf =
+      std::make_shared<pf::SG_1NN_DTWFull<F, L, Strain, Stest>>(tnames, exponents);
+
+    auto sg_1nn_dtw =
+      std::make_shared<pf::SG_1NN_DTW<F, L, Strain, Stest>>(tnames, exponents);
+
+    auto sg_1nn_wdtw =
+      std::make_shared<pf::SG_1NN_WDTW<F, L, Strain, Stest>>(tnames, exponents);
+
+    auto sg_1nn_erp =
+      std::make_shared<pf::SG_1NN_ERP<F, L, Strain, Stest>>(tnames, exponents);
+
+    auto sg_1nn_lcss =
+      std::make_shared<pf::SG_1NN_LCSS<F, L, Strain, Stest>>(tnames);
+
+    auto sg_1nn_msm =
+      std::make_shared<pf::SG_1NN_MSM<F, L, Strain, Stest>>(tnames, msm_costs);
+
+    auto sg_1nn_twe =
+      std::make_shared<pf::SG_1NN_TWE<F, L, Strain, Stest>>(tnames, twe_nus, twe_lambdas);
 
     auto sg_chooser = std::make_shared<pf::SG_chooser<L, Strain, Stest>>(
-      pf::SG_chooser<L, Strain, Stest>::SGVec_t { sg_1nn_da, sg_1nn_dtwf, sg_1nn_dtw, sg_1nn_wdtw, sg_1nn_erp },
-      5 );
+      pf::SG_chooser<L, Strain, Stest>::SGVec_t{
+        //sg_1nn_da,
+        sg_1nn_dtw,
+        //sg_1nn_dtwf,
+        sg_1nn_adtw,
+        //sg_1nn_wdtw,
+        //sg_1nn_erp,
+        sg_1nn_lcss,
+        //sg_1nn_msm,
+        //sg_1nn_twe,
+      }
+      , 5
+    );
 
     // --- --- --- Leaf Generator
     auto sgleaf_purenode = std::make_shared<pf::SGLeaf_PureNode<F, L, Strain, Stest>>();
@@ -267,10 +332,14 @@ int main(int argc, char **argv) {
         }
       }
       std::cout << "1 tree: correct = " << correct << "/" << test_top << std::endl;
+      for (const auto&[n, c] : train_state.selected_distances) {
+        std::cout << n << ": " << c << std::endl;
+      }
+      train_state.selected_distances.clear();
     }
 
     // --- --- --- Forest Trainer
-    pf::PForestTrainer<L, Strain, Stest> forest_trainer(tree_trainer, 5);
+    pf::PForestTrainer<L, Strain, Stest> forest_trainer(tree_trainer, 100);
     {
       std::vector<ByClassMap<L>> train_bcm{std::get<0>(train_dataset.header().get_BCM())};
       auto forest = forest_trainer.train(train_state, train_bcm);
@@ -286,9 +355,11 @@ int main(int argc, char **argv) {
         }
       }
       std::cout << "5 trees: correct = " << correct << "/" << test_top << std::endl;
-
+      for (const auto&[n, c] : train_state.selected_distances) {
+        std::cout << n << ": " << c << std::endl;
+      }
+      train_state.selected_distances.clear();
     }
-
 
   }
 
--- libtempo/classifier/proximity_forest/splitters/distance_splitters.hpp
+++ libtempo/classifier/proximity_forest/splitters/distance_splitters.hpp
@@ -4,10 +4,14 @@
 #include <libtempo/concepts.hpp>
 
 #include <libtempo/distance/direct.hpp>
+#include <libtempo/distance/adtw.hpp>
 #include <libtempo/distance/dtw.hpp>
 #include <libtempo/distance/cdtw.hpp>
 #include <libtempo/distance/wdtw.hpp>
 #include <libtempo/distance/erp.hpp>
+#include <libtempo/distance/lcss.hpp>
+#include <libtempo/distance/msm.hpp>
+#include <libtempo/distance/twe.hpp>
 
 #include <random>
 #include <utility>
@@ -76,17 +80,27 @@ namespace libtempo::classifier::pf {
     template<Float F, Label L, typename Strain, typename Stest> requires has_prng<Strain>
       &&TimeSeriesDataset<Strain, F, L>
     struct TrainSplitter_1NN : public IPF_NodeGenerator<L, Strain, Stest> {
+
       /// Use same distance type as the resulting test splitter
       using distance_t = typename TestSplitter_1NN<F, L, Stest>::distance_t;
+
+      /// Callback function
+      using callback_t = std::function<void(Strain&)>;
+
       /// Shorthand for the result type
       using Result = typename IPF_NodeGenerator<L, Strain, Stest>::Result;
+
       /// Elastic distance between two series - must be already parameterized
       distance_t distance;
+
       /// Transformation name use to access the TimeSeriesDataset
       std::string transformation_name;
 
-      TrainSplitter_1NN(distance_t distance, std::string transformation_name) :
-        distance(std::move(distance)), transformation_name(std::move(transformation_name)) {}
+      /// Callback for the result
+      callback_t cb;
+
+      TrainSplitter_1NN(distance_t distance, std::string transformation_name, callback_t cb = [](Strain&) {}) :
+        distance(std::move(distance)), transformation_name(std::move(transformation_name)), cb(cb) {}
 
       /// Override generate function from interface ISplitterGenerator
       Result generate(Strain& state, const std::vector<ByClassMap<L>>& bcmvec) const override {
@@ -138,11 +152,12 @@ namespace libtempo::classifier::pf {
           v_bcm.emplace_back(std::move(result_bcmvec[idx]));
         }
         // Build the splitter
-        return Result{ResNode<L, Stest>{
+        return Result{ResNode<L, Strain, Stest>{
           .branch_splits = std::move(v_bcm),
           .splitter=std::make_unique<TestSplitter_1NN<F, L, Stest>>(
             train_dataset, train_indexset, labels_to_index, transformation_name, distance
-          )
+          ),
+          .callback = cb
         }};
       }
     };
@@ -182,40 +197,11 @@ namespace libtempo::classifier::pf {
         return distance::directa(t1, t2, distance::univariate::ade<F, TSeries<F, L >>(e), bsf);
       };
 
-      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname).generate(state, bcmvec);
+      auto cb = [](Strain& strain) { strain.selected_distances["da"] += 1; };
+
+      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname, cb).generate(state, bcmvec);
     }
-  };
 
-  /** 1NN DTW with full window Splitter Generator */
-  template<Float F, Label L, typename Strain, typename Stest>
-  struct SG_1NN_DTWFull : public IPF_NodeGenerator<L, Strain, Stest> {
-    // Type shorthands
-    using Result = typename IPF_NodeGenerator<L, Strain, Stest>::Result;
-    using distance_t = typename internal::TestSplitter_1NN<F, L, Stest>::distance_t;
-
-    /// Transformation name
-    std::shared_ptr<std::vector<std::string>> transformation_names;
-
-    /// Exponent used in the cost function
-    std::shared_ptr<std::vector<double>> exponents;
-
-    SG_1NN_DTWFull(std::shared_ptr<std::vector<std::string>> transformation_names,
-                   std::shared_ptr<std::vector<double>> exponents) :
-      transformation_names(std::move(transformation_names)),
-      exponents(std::move(exponents)) {}
-
-    /// Override interface ISplitterGenerator
-    Result generate(Strain& state, const std::vector<ByClassMap<L>>& bcmvec) const override {
-
-      std::string tname = utils::pick_one(*transformation_names, *state.prng);
-      double e = utils::pick_one(*exponents, *state.prng);
-
-      distance_t distance = [e](const TSeries<F, L>& t1, const TSeries<F, L>& t2, double bsf) {
-        return distance::dtw(t1, t2, distance::univariate::ade<F, TSeries<F, L >>(e), bsf);
-      };
-
-      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname).generate(state, bcmvec);
-    }
   };
 
   /** 1NN DTW Splitter Generator */
@@ -250,7 +236,113 @@ namespace libtempo::classifier::pf {
         return distance::cdtw(t1, t2, w, distance::univariate::ade<F, TSeries<F, L >>(e), bsf);
       };
 
-      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname).generate(state, bcmvec);
+      auto cb = [](Strain& strain) { strain.selected_distances["dtw"] += 1; };
+
+      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname, cb).generate(state, bcmvec);
+    }
+  };
+
+  /** 1NN DTW with full window Splitter Generator */
+  template<Float F, Label L, typename Strain, typename Stest>
+  struct SG_1NN_DTWFull : public IPF_NodeGenerator<L, Strain, Stest> {
+    // Type shorthands
+    using Result = typename IPF_NodeGenerator<L, Strain, Stest>::Result;
+    using distance_t = typename internal::TestSplitter_1NN<F, L, Stest>::distance_t;
+
+    /// Transformation name
+    std::shared_ptr<std::vector<std::string>> transformation_names;
+
+    /// Exponent used in the cost function
+    std::shared_ptr<std::vector<double>> exponents;
+
+    SG_1NN_DTWFull(std::shared_ptr<std::vector<std::string>> transformation_names,
+                   std::shared_ptr<std::vector<double>> exponents) :
+      transformation_names(std::move(transformation_names)),
+      exponents(std::move(exponents)) {}
+
+    /// Override interface ISplitterGenerator
+    Result generate(Strain& state, const std::vector<ByClassMap<L>>& bcmvec) const override {
+
+      std::string tname = utils::pick_one(*transformation_names, *state.prng);
+      double e = utils::pick_one(*exponents, *state.prng);
+
+      distance_t distance = [e](const TSeries<F, L>& t1, const TSeries<F, L>& t2, double bsf) {
+        return distance::dtw(t1, t2, distance::univariate::ade<F, TSeries<F, L >>(e), bsf);
+      };
+
+      auto cb = [](Strain& strain) { strain.selected_distances["dtwf"] += 1; };
+
+      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname, cb).generate(state, bcmvec);
+    }
+  };
+
+  /** 1NN ADTW Splitter Generator */
+  template<Float F, Label L, typename Strain, typename Stest>
+  struct SG_1NN_ADTW : public IPF_NodeGenerator<L, Strain, Stest> {
+    // Type shorthands
+    using Result = typename IPF_NodeGenerator<L, Strain, Stest>::Result;
+    using distance_t = typename internal::TestSplitter_1NN<F, L, Stest>::distance_t;
+
+    /// Transformation name
+    std::shared_ptr<std::vector<std::string>> transformation_names;
+
+    /// Exponent used in the cost function
+    std::shared_ptr<std::vector<double>> exponents;
+
+    /// Sampling size when assessing the penalty
+    size_t penalty_sampling_size;
+
+    /// Herrmann Schedule - divisor
+    size_t hs_div;
+
+    /// Herrmann Schedule - exponent
+    double hs_exp;
+
+    SG_1NN_ADTW(std::shared_ptr<std::vector<std::string>> transformation_names,
+                std::shared_ptr<std::vector<double>> exponents,
+                size_t penalty_sampling_size,
+                size_t hs_div,
+                double hs_exp) :
+      transformation_names(std::move(transformation_names)),
+      exponents(std::move(exponents)),
+      penalty_sampling_size(penalty_sampling_size),
+      hs_div(hs_div),
+      hs_exp(hs_exp) {}
+
+    /// Override interface ISplitterGenerator
+    Result generate(Strain& state, const std::vector<ByClassMap<L>>& bcmvec) const override {
+      std::string tname = utils::pick_one(*transformation_names, *state.prng);
+      double e = utils::pick_one(*exponents, *state.prng);
+
+      auto dist = distance::univariate::ade<F, TSeries<F, L >>(e);
+
+      // --- --- --- Sampling
+      const auto& train_dataset_map = *state.dataset_shared_map;
+      const auto& train_dataset = train_dataset_map.at(tname);
+      utils::StddevWelford welford;
+      {
+        for (size_t i = 0; i<penalty_sampling_size; ++i) {
+          const auto& q = utils::pick_one(train_dataset.data(), *state.prng);
+          const auto& s = utils::pick_one(train_dataset.data(), *state.prng);
+          const auto cost = distance::directa<F>(q, s, dist);
+          welford.update(cost);
+        }
+      }
+      const double sampled_mean_dist = welford.get_mean();
+
+      // Get a x in [0, div]
+      const double x = std::uniform_int_distribution<int>(0, hs_div + 1)(*state.prng);
+      const double r = std::pow(x/(double)hs_div, hs_exp);
+      const double omega = r*sampled_mean_dist;
+
+      distance_t distance = [e, omega, dist](const TSeries<F, L>& t1, const TSeries<F, L>& t2, double bsf) {
+        return distance::adtw(t1, t2, omega, dist, bsf);
+      };
+
+      auto cb = [](Strain& strain) { strain.selected_distances["adtw"] += 1; };
+
+      auto sg = internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname, cb);
+      return sg.generate(state, bcmvec);
     }
   };
 
@@ -286,7 +378,9 @@ namespace libtempo::classifier::pf {
         return distance::wdtw(t1, t2, *weights, distance::univariate::ade<F, TSeries<F, L >>(e), bsf);
       };
 
-      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname).generate(state, bcmvec);
+      auto cb = [](Strain& strain) { strain.selected_distances["wdtw"] += 1; };
+
+      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname, cb).generate(state, bcmvec);
     }
   };
 
@@ -317,7 +411,7 @@ namespace libtempo::classifier::pf {
       const size_t win_top = (state.get_header().length_max() + 1)/4;
       const auto w = std::uniform_int_distribution<size_t>(0, win_top)(*state.prng);
 
-      // Compute the gap value using the standard deviation of the data reching this node
+      // Compute the gap value using the standard deviation of the data reaching this node
       const auto& train_dataset_map = *state.dataset_shared_map;
       const auto& train_dataset = train_dataset_map.at(tname);
       auto stddev_ = stddev(train_dataset, IndexSet(bcmvec.back()));
@@ -331,7 +425,127 @@ namespace libtempo::classifier::pf {
         );
       };
 
-      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname).generate(state, bcmvec);
+      auto cb = [](Strain& strain) { strain.selected_distances["erp"] += 1; };
+
+      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname, cb).generate(state, bcmvec);
+    }
+  };
+
+  /** 1NN LCSS Splitter Generator */
+  template<Float F, Label L, typename Strain, typename Stest>
+  struct SG_1NN_LCSS : public IPF_NodeGenerator<L, Strain, Stest> {
+    // Type shorthands
+    using Result = typename IPF_NodeGenerator<L, Strain, Stest>::Result;
+    using distance_t = typename internal::TestSplitter_1NN<F, L, Stest>::distance_t;
+
+    /// Transformation name
+    std::shared_ptr<std::vector<std::string>> transformation_names;
+
+    explicit SG_1NN_LCSS(std::shared_ptr<std::vector<std::string>> transformation_names) :
+      transformation_names(std::move(transformation_names)) {}
+
+    /// Override interface ISplitterGenerator
+    Result generate(Strain& state, const std::vector<ByClassMap<L>>& bcmvec) const override {
+      std::string tname = utils::pick_one(*transformation_names, *state.prng);
+
+      // Compute the window
+      const size_t win_top = (state.get_header().length_max() + 1)/4;
+      const auto w = std::uniform_int_distribution<size_t>(0, win_top)(*state.prng);
+
+      // Compute the epsilon value using the standard deviation of the data reaching this node
+      const auto& train_dataset_map = *state.dataset_shared_map;
+      const auto& train_dataset = train_dataset_map.at(tname);
+      auto stddev_ = stddev(train_dataset, IndexSet(bcmvec.back()));
+      const double epsilon = std::uniform_real_distribution<double>(0.2*stddev_, stddev_)(*state.prng);
+
+      distance_t distance = [w, epsilon](const TSeries<F, L>& t1, const TSeries<F, L>& t2, double bsf) {
+        return distance::univariate::lcss(t1, t2, w, epsilon, bsf);
+      };
+
+      auto cb = [](Strain& strain) { strain.selected_distances["lcss"] += 1; };
+
+      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname, cb).generate(state, bcmvec);
+    }
+  };
+
+  /** 1NN MSM Splitter Generator */
+  template<Float F, Label L, typename Strain, typename Stest>
+  struct SG_1NN_MSM : public IPF_NodeGenerator<L, Strain, Stest> {
+    // Type shorthands
+    using Result = typename IPF_NodeGenerator<L, Strain, Stest>::Result;
+    using distance_t = typename internal::TestSplitter_1NN<F, L, Stest>::distance_t;
+
+    /// Transformation name
+    std::shared_ptr<std::vector<std::string>> transformation_names;
+
+    /// Set of possible costs
+    std::shared_ptr<std::vector<double>> costs;
+
+    SG_1NN_MSM(
+      std::shared_ptr<std::vector<std::string>> transformation_names,
+      std::shared_ptr<std::vector<double>> costs
+    ) :
+      transformation_names(std::move(transformation_names)),
+      costs(std::move(costs)) {}
+
+    /// Override interface ISplitterGenerator
+    Result generate(Strain& state, const std::vector<ByClassMap<L>>& bcmvec) const override {
+      std::string tname = utils::pick_one(*transformation_names, *state.prng);
+
+      // Compute the cost
+      auto c = utils::pick_one(*costs, *state.prng);
+
+      distance_t distance = [c](const TSeries<F, L>& t1, const TSeries<F, L>& t2, double bsf) {
+        return distance::univariate::msm(t1, t2, c, bsf);
+      };
+
+      auto cb = [](Strain& strain) { strain.selected_distances["msm"] += 1; };
+
+      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname, cb).generate(state, bcmvec);
+    }
+  };
+
+  /** 1NN TWE Splitter Generator */
+  template<Float F, Label L, typename Strain, typename Stest>
+  struct SG_1NN_TWE : public IPF_NodeGenerator<L, Strain, Stest> {
+    // Type shorthands
+    using Result = typename IPF_NodeGenerator<L, Strain, Stest>::Result;
+    using distance_t = typename internal::TestSplitter_1NN<F, L, Stest>::distance_t;
+
+    /// Transformation name
+    std::shared_ptr<std::vector<std::string>> transformation_names;
+
+    /// Set of possible nu
+    std::shared_ptr<std::vector<double>> nus;
+
+    /// Set of possible lambda
+    std::shared_ptr<std::vector<double>> lambdas;
+
+    SG_1NN_TWE(
+      std::shared_ptr<std::vector<std::string>> transformation_names,
+      std::shared_ptr<std::vector<double>> nus,
+      std::shared_ptr<std::vector<double>> lambdas
+    ) :
+      transformation_names(std::move(transformation_names)),
+      nus(std::move(nus)),
+      lambdas(std::move(lambdas))
+      {}
+
+    /// Override interface ISplitterGenerator
+    Result generate(Strain& state, const std::vector<ByClassMap<L>>& bcmvec) const override {
+      std::string tname = utils::pick_one(*transformation_names, *state.prng);
+
+      // Pick nu and lambda
+      auto nu = utils::pick_one(*nus, *state.prng);
+      auto lambda = utils::pick_one(*lambdas, *state.prng);
+
+      distance_t distance = [nu, lambda](const TSeries<F, L>& t1, const TSeries<F, L>& t2, double bsf) {
+        return distance::univariate::twe<F, TSeries<F,L>>(t1, t2, nu, lambda, bsf);
+      };
+
+      auto cb = [](Strain& strain) { strain.selected_distances["twe"] += 1; };
+
+      return internal::TrainSplitter_1NN<F, L, Strain, Stest>(distance, tname, cb).generate(state, bcmvec);
     }
   };
 

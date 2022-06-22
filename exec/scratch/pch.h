445ed2727bad995b3842d340fbedd4d470cc91fe
HerrmannM <HerrmannM@users.noreply.github.com>
2022-06-22 15:24:24 +1000

    add scratch base exec

---
+++ exec/scratch/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <tempo/utils/utils.hpp>
+#include <tempo/distance/elastic/dtw.hpp>
+#include <tempo/distance/lockstep/direct.hpp>
+#include <tempo/transform/normalization.hpp>
+

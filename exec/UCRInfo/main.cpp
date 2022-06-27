330f1769413cf093f228d21fba3a57992a94faa7
HerrmannM <HerrmannM@users.noreply.github.com>
2022-06-27 12:40:06 +1000

    Update stats generation: allow several modified minkowski exponent in 1 run

--- exec/UCRInfo/main.cpp
+++ exec/UCRInfo/main.cpp
@@ -1,11 +1,66 @@
 #include "pch.h"
+#include <typeinfo>
 #include <tempo/reader/new_reader.hpp>
 
 #include "cli.hpp"
 
 namespace fs = std::filesystem;
 
+struct config {
+  // Modified Minkowski
+  int mm_nbsample{0};
+  std::vector<double> mm_exponents{};
+};
+
 // Given a UCR dataset in ts format, compute some information
+Json::Value compute_info(
+  tempo::DatasetHeader const& header,
+  tempo::DTS const& dts,
+  config const& conf,
+  tempo::PRNG& prng
+) {
+  using namespace tempo;
+  // --- Header info
+  Json::Value v = header.to_json();
+  // --- Stats
+  {
+    Json::Value statsv;
+    DTS_Stats stats(dts);
+    statsv["min"] = utils::to_json(stats._min);
+    statsv["max"] = utils::to_json(stats._max);
+    statsv["mean"] = utils::to_json(stats._mean);
+    statsv["stddev"] = utils::to_json(stats._stddev);
+    v["stats"] = std::move(statsv);
+  }
+  // --- Sample
+  if (conf.mm_nbsample>0) {
+    Json::Value mmv;
+    if (header.has_missing_value()) { mmv = Json::Value("error_missing_value"); }
+    else if (header.nb_dimensions()!=1) { mmv = Json::Value("error_multivariate"); }
+    else {
+      mmv["results"] = Json::arrayValue;
+      for (const double mm_exponent : conf.mm_exponents) {
+        Json::Value emmv;
+        emmv["sample_size"] = conf.mm_nbsample;
+        emmv["minkowki_exponent"] = mm_exponent;
+        utils::StddevWelford welford;
+        size_t j = 0;
+        std::uniform_int_distribution<> distrib(0, (int)dts.size() - 1);
+        for (int i = 0; i<conf.mm_nbsample; ++i) {
+          const auto& q = dts[distrib(prng)];
+          const auto& s = dts[distrib(prng)];
+          const auto& cost = tempo::distance::minkowski(q, s, mm_exponent);
+          welford.update(cost);
+        }
+        emmv["mean"] = welford.get_mean();
+        emmv["stddev"] = welford.get_stddev_s();
+        mmv["results"].append(std::move(emmv));
+      }
+    }
+    v["modminkowki_sample"] = std::move(mmv);
+  }
+  return v;
+}
 
 int main(int argc, char **argv) {
   using namespace std;
@@ -17,14 +72,15 @@ int main(int argc, char **argv) {
   string program_name(*argv);
   vector<string> args(argv + 1, argv + argc);
   if (args.empty()) { do_exit(0, usage); }
+  config conf;
 
   // --- --- ---
   // Optional
 
   // Random seed for sampling
-  size_t seed;
+  long long seed;
   {
-    auto p_seed = tempo::scli::get_parameter<long long>(args, "-seed", tempo::scli::extract_int, 0);
+    auto p_seed = tempo::scli::get_parameter<long long>(args, "-seed", tempo::scli::extract_int, -1);
     if (p_seed<0) { seed = std::random_device()(); } else { seed = p_seed; }
   }
   tempo::PRNG prng(seed);
@@ -37,22 +93,23 @@ int main(int argc, char **argv) {
   }
 
   // Sample with Modified Minkowsky
-  size_t mm_nbsample{0};
-  double mm_exponent{0.0};
   {
     auto parg_dist = tempo::scli::get_parameter<string>(args, "-modminkowski", tempo::scli::extract_string);
     if (parg_dist) {
       auto v = tempo::reader::split(parg_dist.value(), ':');
-      bool ok = v.size()==2;
+      bool ok = v.size()>=2;
       if (ok) {
         auto on = tempo::reader::as_int(v[0]);
-        auto oe = tempo::reader::as_double(v[1]);
-        ok = oe.has_value()&&oe.has_value();
-        if (ok) {
-          mm_nbsample = on.value();
-          mm_exponent = oe.value();
+        ok = (bool)on;
+        if (ok) { conf.mm_nbsample = on.value(); }
+        //
+        for (size_t i = 1; ok&&i<v.size(); ++i) {
+          auto oe = tempo::reader::as_double(v[i]);
+          ok = (bool)oe;
+          if (ok) { conf.mm_exponents.push_back(oe.value()); }
         }
-      } else { do_exit(1, "specify <n>:<e> after '-modminkowski'"); }
+      }
+      if (!ok) { do_exit(1, "specify <n>:<e0>:...:<en> after '-modminkowski'"); }
     }
   }
 
@@ -91,64 +148,22 @@ int main(int argc, char **argv) {
   // --- --- --- --- --- ---
   // Json Record
   Json::Value jv;
+  jv["prng_type"] = std::string(typeid(PRNG).name());
+  jv["prng_seed"] = std::to_string(seed);
   jv["dataset"] = train_header.name();
 
   // --- --- --- --- --- ---
-  // Analyse train
-  {
-    // --- Header info
-    Json::Value v = train_header.to_json();
-    v.removeMember("name");
-    // --- Stats
-    {
-      Json::Value statsv;
-      DTS_Stats stats(train_split);
-      statsv["min"] = utils::to_json(stats._min);
-      statsv["max"] = utils::to_json(stats._max);
-      statsv["mean"] = utils::to_json(stats._mean);
-      statsv["stddev"] = utils::to_json(stats._stddev);
-      v["stats"] = std::move(statsv);
-    }
-    // --- Sample
-    if (mm_nbsample>0) {
-      Json::Value mmv;
-      if (train_header.has_missing_value()) {
-        mmv = Json::Value("error_missing_value");
-      } else if(train_header.nb_dimensions() != 1) {
-        mmv = Json::Value("error_multivariate");
-      } else {
-        mmv["size"] = mm_nbsample;
-        utils::StddevWelford welford;
-        size_t j = 0;
-        std::uniform_int_distribution<> distrib(0, (int)train_split.size() - 1);
-        for (size_t i = 0; i<mm_nbsample; ++i) {
-          const auto& q = train_split[distrib(prng)];
-          const auto& s = train_split[distrib(prng)];
-          const auto& cost = tempo::distance::minkowski(q, s, mm_exponent);
-          welford.update(cost);
-        }
-        mmv["mean"] = welford.get_mean();
-        mmv["stddev"] = welford.get_stddev_s();
-        mmv["variance"] = welford.get_variance_s();
-      }
-      v["modminkowki_sample"] = std::move(mmv);
-    }
-    // --- Return
-    jv["train"] = std::move(v);
 
+  {
+    Json::Value train_v = compute_info(train_header, train_split, conf, prng);
+    train_v.removeMember("name");
+    jv["train"] = std::move(train_v);
   }
   {
-    Json::Value v = test_header.to_json();
-    v.removeMember("name");
-    jv["test"] = std::move(v);
+    Json::Value test_v = compute_info(test_header, test_split, conf, prng);
+    test_v.removeMember("name");
+    jv["test"] = std::move(test_v);
   }
-
-  // --- --- --- --- --- ---
-  // Modified Minkowski Sampling
-
-
-  // --- --- --- --- --- ---
-  // Stats on train and test
 
   cout << endl << jv.toStyledString() << endl;
   if (outpath) {

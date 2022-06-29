9d6868ba7e3b1a2bec7c27b3de74e2941025ee4a
HerrmannM <HerrmannM@users.noreply.github.com>
2022-06-29 11:52:16 +1000

    Fix inline

--- src/tempo/distance/sliding/cross_correlation.hpp
+++ src/tempo/distance/sliding/cross_correlation.hpp
@@ -10,7 +10,7 @@ namespace tempo::distance {
 
     /// Cross Correlation sequence computation using FFT with a length adjusted to a power of 2 for efficiency.
     /// Based on https://github.com/johnpaparrizos/TSDistEval/blob/master/slidingmeasures/NCC.m
-    arma::Row<F> cc_seq(arma::Row<F> const& A, arma::Row<F> const& B) {
+    inline arma::Row<F> cc_seq(arma::Row<F> const& A, arma::Row<F> const& B) {
       size_t len = std::max(A.n_elem, B.n_elem);
       size_t fftlenght = 1 << utils::nextpow2<size_t>(2*len - 1); // 1<<p  == 2^p for unsigned integral
       arma::Row<F> r = arma::real(arma::ifft(arma::fft(A, fftlenght)%arma::conj(arma::fft(B, fftlenght))));
@@ -18,13 +18,13 @@ namespace tempo::distance {
     }
 
     /// Maximum Cross Correlation value
-    F max_cc(arma::Row<F> const& A, arma::Row<F> const& B) {
+    inline F max_cc(arma::Row<F> const& A, arma::Row<F> const& B) {
       return arma::max(cc_seq(A, B));
     }
 
     /// Maximum Biased Normalized Cross Correlation value
     /// Divides the result of 'max_cc(A,B)' by max(len(A), len(B)) (max vector length)
-    F max_ncc_b(arma::Row<F> const& A, arma::Row<F> const& B) {
+    inline F max_ncc_b(arma::Row<F> const& A, arma::Row<F> const& B) {
       F n = std::max<F>(A.n_elem, B.n_elem);
       return arma::max(cc_seq(A, B))/n;
     }
@@ -32,7 +32,7 @@ namespace tempo::distance {
     /// Maximum Coefficient Normalized Cross Correlation value
     /// Divides the result of 'max_cc(A,B)' by the ||A||*||B|| (multiplication of the norms)
     /// Return a value in the [-1, 1] range
-    F max_ncc_c(arma::Row<F> const& A, arma::Row<F> const& B) {
+    inline F max_ncc_c(arma::Row<F> const& A, arma::Row<F> const& B) {
       F n = arma::norm(A)*arma::norm(B);
       return arma::max(cc_seq(A, B))/n;
     }
@@ -43,7 +43,7 @@ namespace tempo::distance {
   /// k-Shape: Efficient and Accurate Clustering of Time Series, John Paparrizos & Luis Gravano, SIGMOD 2015
   /// Result is in [0, 2], "with 0 indicating perfect similarity".
   /// Warning: due to numerical approximation, SBD(A, A) returns a value close to 0 but not 0!
-  F SBD(arma::Row<F> const& A, arma::Row<F> const& B) {
+  inline F SBD(arma::Row<F> const& A, arma::Row<F> const& B) {
     return ((F)1) - cross_correlation::max_ncc_c(A, B);
   }
 

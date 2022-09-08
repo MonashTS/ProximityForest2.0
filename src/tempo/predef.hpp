9a78e4055892dd279a3e2d4c052e719a423e6e23
HerrmannM <HerrmannM@users.noreply.github.com>
2022-09-08 17:50:52 +1000

    Cleanup predefinition and precompiled header

--- src/tempo/predef.hpp
+++ src/tempo/predef.hpp
@@ -1,6 +1,43 @@
 #pragma once
 
-#include "pch_lib.hpp"
+// STD C LIB
+#include <cassert>
+#include <cmath>
+#include <cstddef>
+#include <cstdlib>
+
+// STD C++ LIB
+#include <algorithm>
+#include <any>
+#include <chrono>
+#include <concepts>
+#include <filesystem>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <memory>
+#include <map>
+#include <mutex>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <utility>
+#include <variant>
+#include <vector>
+
+// MLPACK which also gives us Armadillo
+#include <mlpack/core.hpp>
+
+// JSONCPP
+#include <json/json.h>
 
 namespace tempo {
 

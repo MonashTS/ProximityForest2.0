5bade3cec31d1ca5c874e80f4278018394e510b7
HerrmannM <HerrmannM@users.noreply.github.com>
2022-11-08 10:08:26 +1100

    Cleanup DA splitter

--- src/tempo/classifier/TSChief/snode/nn1splitter/nn1_directa.hpp
+++ src/tempo/classifier/TSChief/snode/nn1splitter/nn1_directa.hpp
@@ -11,27 +11,20 @@ namespace tempo::classifier::TSChief::snode::nn1splitter {
   struct DA : public BaseDist {
     F cfe;
 
-    DA(std::string tname, F cfe) : BaseDist(std::move(tname)), cfe(cfe) {}
+    DA(std::string tname, F cfe);
 
-    F eval(const TSeries& t1, const TSeries& t2, F bsf) override {
-      return distance::univariate::directa(t1, t2, cfe, bsf);
-    }
+    F eval(const TSeries& t1, const TSeries& t2, F bsf) override;
 
-    std::string get_distance_name() override { return "DA:" + std::to_string(cfe); }
+    std::string get_distance_name() override;
   };
 
   struct DAGen : public i_GenDist {
     TransformGetter get_transform;
     ExponentGetter get_cfe;
 
-    DAGen(TransformGetter gt, ExponentGetter ge) :
-      get_transform(std::move(gt)), get_cfe(std::move(ge)) {}
+    DAGen(TransformGetter gt, ExponentGetter ge);
 
-    std::unique_ptr<i_Dist> generate(TreeState& state, TreeData const& /*data*/, const ByClassMap& /* bcm */) override {
-      const std::string tn = get_transform(state);
-      const F e = get_cfe(state);
-      return std::make_unique<DA>(tn, e);
-    }
+    std::unique_ptr<i_Dist> generate(TreeState& state, TreeData const& /*data*/, const ByClassMap& /* bcm */) override;
   };
 
 } // End of namespace tempo::classifier::TSChief::snode::nn1splitter

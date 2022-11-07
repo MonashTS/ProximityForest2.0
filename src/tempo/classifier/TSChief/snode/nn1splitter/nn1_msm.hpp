4dbe5185eebfd56ac2b94ecfb0b660075d4f1955
HerrmannM <HerrmannM@users.noreply.github.com>
2022-11-08 10:39:53 +1100

    Cleanup MSM splitter

--- src/tempo/classifier/TSChief/snode/nn1splitter/nn1_msm.hpp
+++ src/tempo/classifier/TSChief/snode/nn1splitter/nn1_msm.hpp
@@ -1,9 +1,5 @@
 #pragma once
 
-#include <tempo/utils/utils.hpp>
-#include <tempo/dataset/tseries.hpp>
-#include <tempo/distance/tseries.univariate.hpp>
-
 #include "nn1dist_base.hpp"
 
 namespace tempo::classifier::TSChief::snode::nn1splitter {
@@ -11,27 +7,20 @@ namespace tempo::classifier::TSChief::snode::nn1splitter {
   struct MSM : public BaseDist {
     F cost;
 
-    MSM(std::string tname, F cost) : BaseDist(std::move(tname)), cost(cost) {}
+    MSM(std::string tname, F cost);
 
-    F eval(const TSeries& t1, const TSeries& t2, F bsf) override {
-      return distance::univariate::msm(t1, t2, cost, bsf);
-    }
+    F eval(const TSeries& t1, const TSeries& t2, F bsf) override;
 
-    std::string get_distance_name() override { return "MSM:" + std::to_string(cost); }
+    std::string get_distance_name() override;
   };
 
   struct MSMGen : public i_GenDist {
     TransformGetter get_transform;
     T_GetterState<F> get_cost;
 
-    MSMGen(TransformGetter get_transform, T_GetterState<F> get_cost) :
-      get_transform(std::move(get_transform)), get_cost(std::move(get_cost)) {}
+    MSMGen(TransformGetter get_transform, T_GetterState<F> get_cost);
 
-    std::unique_ptr<i_Dist> generate(TreeState& state, TreeData const& /* d */, const ByClassMap& /* bcm */) override {
-      const std::string tn = get_transform(state);
-      const F cost = get_cost(state);
-      return std::make_unique<MSM>(tn, cost);
-    }
+    std::unique_ptr<i_Dist> generate(TreeState& state, TreeData const& /* d */, const ByClassMap& /* bcm */) override;
   };
 
 } // End of namespace tempo::classifier::TSChief::snode::nn1splitter
